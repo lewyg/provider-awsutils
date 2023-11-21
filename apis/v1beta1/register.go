3ab9ca6031477e5cc1b853dc65cbd6eed3848e9f
Grzegorz Lewczuk <extern.lewczuk_grzegorz@allianz.de>
2023-11-21 19:53:26 +0100

    init with the name

--- apis/v1beta1/register.go
+++ apis/v1beta1/register.go
@@ -13,7 +13,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "template.upbound.io"
+	Group   = "awsutils.crossplane.io"
 	Version = "v1beta1"
 )
 
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/upbound/upjet-provider-template
+module github.com/crossplane-contrib/provider-awsutils
 
 go 1.19
 
